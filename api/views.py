3a3f4cf6e59d4b6d25ef0e67c522378d91a7f8a3
Israel Fernández <israel1394@gmail.com>
2017-09-19 11:37:31 -0500

    added imports to views :c

--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Notes
+from .serializers import NoteSerializer
 # Create your views here.
 
 @api_view(['GET', 'POST'])
